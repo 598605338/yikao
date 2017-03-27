41c661e06b56b162b9263c1d2262118c10f8c430
598605338 <598603558@qq.com>
2017-03-27 18:56:00 +0800

    添加院校录取信息模块

---
+++ src/main/java/com/linjia/web/dao/AdmissionInfoMapper.java
@@ -0,0 +1,7 @@
+package com.linjia.web.dao;
+
+import com.linjia.base.dao.BaseDao;
+import com.linjia.web.model.AdmissionInfo;
+
+public interface AdmissionInfoMapper extends BaseDao<AdmissionInfo, Long> {
+}
---
+++ src/main/java/com/linjia/web/dao/AdmissionRuleMapper.java
@@ -0,0 +1,7 @@
+package com.linjia.web.dao;
+
+import com.linjia.base.dao.BaseDao;
+import com.linjia.web.model.AdmissionRule;
+
+public interface AdmissionRuleMapper extends BaseDao<AdmissionRule, Long> {
+}
